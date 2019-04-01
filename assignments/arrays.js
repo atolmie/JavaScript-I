be058ef3d6f52c690d9fcb79b2337724e39e831c
atolmie <atolmie22@gmail.com>
2019-04-01 18:19:52 -0500

    array 3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,8 +74,9 @@ console.log(lastCar.car_make, lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = inventory [inventory.sort];
-console.log(carModels.car_model); 
+let carModels = inventory.map(car => car.car_model);
+let carModelsSorted = carModels.sort();
+console.log(carModelsSorted); 
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
